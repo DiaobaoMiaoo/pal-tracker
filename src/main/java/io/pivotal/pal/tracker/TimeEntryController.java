a0f7476f2ccffb7cd0039065414561d2e3df6fd0
Ke MA <ke.ma@rbc.com>
2019-10-01 16:37:38 -0400

    All passed expect the API-update-test

--- src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
@@ -1,30 +1,62 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RestController
 public class TimeEntryController {
+
+//    @Autowired
+    TimeEntryRepository timeEntryRepository;
+
     public TimeEntryController(TimeEntryRepository timeEntryRepository) {
+        this.timeEntryRepository = timeEntryRepository;
     }
 
-    public ResponseEntity create(TimeEntry timeEntryToCreate) {
-        return null;
+    @PostMapping(path = "/time-entries", consumes = "application/json")
+    public ResponseEntity create(@RequestBody TimeEntry timeEntryToCreate) {
+        TimeEntry timeEntry = timeEntryRepository.create(timeEntryToCreate);
+        ResponseEntity<TimeEntry> responseEntity = new ResponseEntity<TimeEntry>(timeEntry, HttpStatus.CREATED);
+        return responseEntity;
     }
 
-    public ResponseEntity<TimeEntry> read(long timeEntryId) {
-        return null;
+    @GetMapping("/time-entries/{timeEntryId}")
+    public ResponseEntity<TimeEntry> read(@PathVariable long timeEntryId) {
+        TimeEntry timeEntry = timeEntryRepository.find(timeEntryId);
+        if (timeEntry == null) {
+            ResponseEntity<TimeEntry> responseEntity = new ResponseEntity<TimeEntry>(HttpStatus.NOT_FOUND);
+            return responseEntity;
+        } else {
+            ResponseEntity<TimeEntry> responseEntity = new ResponseEntity<TimeEntry>(timeEntry, HttpStatus.OK);
+            return responseEntity;
+        }
     }
 
+    @GetMapping("/time-entries")
     public ResponseEntity<List<TimeEntry>> list() {
-        return null;
+        ResponseEntity<List<TimeEntry>> responseEntity = new ResponseEntity<List<TimeEntry>>(timeEntryRepository.list(), HttpStatus.OK);
+        return responseEntity;
     }
 
-    public ResponseEntity update(long timeEntryId, TimeEntry expected) {
-        return null;
+    @PutMapping("/time-entries/{timeEntryId}")
+    public ResponseEntity update(@RequestBody @ModelAttribute("timeEntryId") long timeEntryId, TimeEntry timeEntry) {
+        TimeEntry updatedtTimeEntry = timeEntryRepository.update(timeEntryId, timeEntry);
+        if (updatedtTimeEntry == null) {
+            ResponseEntity<TimeEntry> responseEntity = new ResponseEntity<TimeEntry>(HttpStatus.NOT_FOUND);
+            return responseEntity;
+        } else {
+            ResponseEntity<TimeEntry> responseEntity = new ResponseEntity<TimeEntry>(updatedtTimeEntry, HttpStatus.OK);
+            return responseEntity;
+        }
     }
 
-    public ResponseEntity delete(long timeEntryId) {
-        return null;
+    @DeleteMapping("/time-entries/{timeEntryId}")
+    public ResponseEntity delete(@PathVariable long timeEntryId) {
+        timeEntryRepository.delete(timeEntryId);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 }
--- src/main/java/io/pivotal/pal/tracker/TimeEntryRepository.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryRepository.java
@@ -1,23 +1,48 @@
 package io.pivotal.pal.tracker;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class TimeEntryRepository {
+    private Map<Long, TimeEntry> timeEntryMap = new HashMap<>();
 
+    private Long idCounter = 1L;
 
-    public void create(TimeEntry any) {
-
+    public TimeEntry create(TimeEntry timeEntry) {
+        timeEntryMap.put(idCounter,timeEntry);
+        timeEntry.setId(idCounter);
+        idCounter++;
+        return timeEntry;
     }
 
-    public void find(long timeEntryId) {
-
+    public TimeEntry find(long id) {
+        return timeEntryMap.get(id);
     }
 
-    public void list() {
-
+    public List<TimeEntry> list() {
+        return new ArrayList<>(timeEntryMap.values());
     }
 
-    public void update(long eq, TimeEntry any) {
+    public TimeEntry update(long id, TimeEntry timeEntry) {
+        if(timeEntryMap.get(id) == null) return null;
+        timeEntryMap.put(id, timeEntry);
+        timeEntry.setId(id);
+        return timeEntry;
     }
 
-    public void delete(long timeEntryId) {
+    public void delete(long id) {
+        timeEntryMap.remove(id);
     }
+
+//    public TimeEntry create(TimeEntry any);
+//
+//    public TimeEntry find(long timeEntryId);
+//
+//    public List<TimeEntry> list();
+//
+//    public TimeEntry update(long eq, TimeEntry any);
+//
+//    public void delete(long timeEntryId);
 }
